ac2f020d647fba493f2e5c0969e98b5ed5a09660
Jennifer Nord <5363215+JenniNord@users.noreply.github.com>
2019-10-07 19:13:55 +0200

    Created ReusaxCorp.java
    Initializes an ArrayList of employees when created.
    
    Also has a main method so we can run/test the methods we write.

---
+++ src/assign3/ReusaxCorp.java
@@ -0,0 +1,25 @@
+package assign3;
+
+import java.util.ArrayList;
+
+public class ReusaxCorp {
+    ArrayList<Employee> employees;
+
+    public ReusaxCorp() {
+        employees = new ArrayList<>();
+    }
+
+    //Add methods to register, remove, retrieve, update employees
+    //Add method for calculating the total expenses in by paying:
+    //Gross salaries
+    //Net salaries
+    //Also add methods to Inform the total number of employees registered
+
+    /**
+     * Method to test/run ReusaxCorp
+     * Might move this to a separate class later on depending on how big it becomes
+     */
+    public static void main(String[] args) {
+        ReusaxCorp reusaxCorp = new ReusaxCorp();
+    }
+}
