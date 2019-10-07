802ba932c4351587a91e88bbe7086a94a051f665
Jennifer Nord <5363215+JenniNord@users.noreply.github.com>
2019-10-07 18:50:30 +0200

    Created Employee
    Added attributes (id, name, gross salary) and get/set for these and a method that returns & calculates the net salary.

--- src/assign3/Employee.java
+++ src/assign3/Employee.java
@@ -1,4 +1,49 @@
 package assign3;
 
 public class Employee {
+    private String id;
+    private String name;
+    private double grossSalary;
+
+    public Employee(String id, String name, double grossSalary) {
+        this.id = id;
+        this.name = name;
+        this.grossSalary = grossSalary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGrossSalary() {
+        return grossSalary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGrossSalary(double grossSalary) {
+        this.grossSalary = grossSalary;
+    }
+
+    /**
+     * Regular employees has a net income by paying 10% of taxes in his/her gross salary.
+     */
+    public double getNetSalary() {
+        double netSalary = grossSalary - (grossSalary * 0.1);
+        return netSalary;
+    }
+
+    /**
+     * We might need to format grossSalary so we don't get too many decimals!
+     */
+    public String toString() {
+        String result = name + "'s gross salary is of " + grossSalary + " SEK per month.";
+        return result;
+    }
 }
